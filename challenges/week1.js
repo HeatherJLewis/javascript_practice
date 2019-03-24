9cc4f11f91dcbec0d032365af37781af1e8790b1
HeatherJLewis <heather@heatherlewis.co.uk>
2019-03-24 20:40:58 +0000

    addVAT function completed

--- challenges/week1.js
+++ challenges/week1.js
@@ -17,7 +17,8 @@ function generateInitials(firstName, lastName) {
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  let multiplier = (vatRate/100)+1;
+  return originalPrice * multiplier;
 }
 
 function getSalePrice(originalPrice, reduction) {
